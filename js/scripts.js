815670cb22a2703441030328cabd988d2dc7894f
kemma_u <kemma_u@users.noreply.github.com>
2020-08-18 16:57:26 -0400

    js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hi! Welcome to My Portfolio Site");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
