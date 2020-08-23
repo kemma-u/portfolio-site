d6e940bf184522562e6947c8d2733b975cefc184
kemma_u <kemma_u@users.noreply.github.com>
2020-08-22 20:01:25 -0400

    pre hamburger updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 console.log("Hi! Welcome to My Portfolio Site");
 
+$('#heading-text').text('How to create a hamburger menu with jQuery');
+
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
-} else {
-  x.className = 'navtoggle';
-}
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
